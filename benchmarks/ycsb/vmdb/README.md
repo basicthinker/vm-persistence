22f2d12ca2292307e58332be5111bd1b3be8bf33
Jinglei Ren <jinglei.ren@gmail.com>
2014-11-05 05:58:30 -0800

    Implements VMDB binding for YCSB.

--- benchmarks/ycsb/vmdb/README.md
+++ benchmarks/ycsb/vmdb/README.md
@@ -2,21 +2,21 @@
 
 This section describes how to run YCSB on VMDB.
 
-### 1. Set Up YCSB
-
-Git clone YCSB and compile:
+### 1. Compile VMDB
 
     git clone https://github.com/basicthinker/vm-persistence.git
+    cd vm-persistence/benchmarks/vmdb
+    make ycsb
+    export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
+
+### 2. Set Up YCSB
+
     cd vm-persistence/benchmarks/ycsb
     mvn -pl com.yahoo.ycsb:core,com.yahoo.ycsb:vmdb-binding clean package
 
-### 2. Load data and run tests
+### 3. Load data and run tests
 
-Load the data:
+Example workload test:
 
-    ./bin/ycsb load vmdb -s -P workloads/workloada > outputLoad.txt
-
-Run the workload test:
-
-    ./bin/ycsb run vmdb -s -P workloads/workloada > outputRun.txt
+    ./bin/ycsb run vmdb -s -P workloads/workloadd > outputRun.txt
     
--- benchmarks/ycsb/vmdb/src/main/java/com/yahoo/ycsb/db/VMDBClient.java
+++ benchmarks/ycsb/vmdb/src/main/java/com/yahoo/ycsb/db/VMDBClient.java
@@ -5,7 +5,9 @@ import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.StringByteIterator;
 
+import java.util.Map;
 import java.util.HashMap;
+import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 
@@ -15,37 +17,79 @@ import java.util.Vector;
  * Scanning operations are not supported.
  */
 public class VMDBClient extends DB {
+  static {
+    try {
+      System.loadLibrary("vmdb");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. " + e);
+      System.exit(1);
+    }
+  }
+
+  private static final VMDatabase vmdb = new VMDatabase();
+
+  public static final int OK = 0;
   public static final int ERROR = -1;
-
-  public void init() throws DBException {
-  }
-
-  public void cleanup() throws DBException {
-  }
 
   @Override
   public int read(String table, String key, Set<String> fields,
       HashMap<String, ByteIterator> result) {
-    return 1;
+    if (fields == null) {
+      String[] resultArray = vmdb.Read(table + key, null);
+      int len = resultArray.length / 2;
+      for (int i = 0; i < len; ++i) {
+        result.put(resultArray[i],
+            new StringByteIterator(resultArray[len + i]));
+      }
+    } else {
+      String[] fieldArray = new String[fields.size()];
+      fields.toArray(fieldArray);
+
+      String[] valueArray = vmdb.Read(table + key, fieldArray);
+
+      assert fieldArray.length == valueArray.length;
+      for (int i = 0; i < fieldArray.length; ++i) {
+        result.put(fieldArray[i],
+            new StringByteIterator(valueArray[i]));
+      }
+    }
+    return result.isEmpty() ? ERROR : OK;
   }
 
   @Override
   public int insert(String table, String key,
       HashMap<String, ByteIterator> values) {
-    HashMap<String, String> valueMap = StringByteIterator.getStringMap(values);
-    return 1;
+    String[] fieldArray = new String[values.size()];
+    String[] valueArray = new String[values.size()];
+    int i = 0;
+    for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
+      fieldArray[i] = entry.getKey();
+      valueArray[i] = entry.getValue().toString();
+      ++i;
+    }
+    int n = vmdb.Insert(table + key, fieldArray, valueArray);
+    return n > 0 ? OK : ERROR;
   }
 
   @Override
   public int delete(String table, String key) {
-    return 1;
+    int n = vmdb.Delete(table + key);
+    return n > 0 ? OK : ERROR;
   }
 
   @Override
   public int update(String table, String key,
       HashMap<String, ByteIterator> values) {
-    HashMap<String, String> valueMap = StringByteIterator.getStringMap(values);
-    return 1;
+    String[] fieldArray = new String[values.size()];
+    String[] valueArray = new String[values.size()];
+    int i = 0;
+    for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
+      fieldArray[i] = entry.getKey();
+      valueArray[i] = entry.getValue().toString();
+      ++i;
+    }
+    int n = vmdb.Update(table + key, fieldArray, valueArray);
+    return n > 0 ? OK : ERROR;
   }
 
   @Override
