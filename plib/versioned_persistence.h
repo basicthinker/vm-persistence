3b1b6fd167ebeec95c807c830c9adf73ee55e599
Jinglei Ren <jinglei@ren.systems>
2015-04-04 20:28:19 -0700

    [plib] Init versioned persistence interface.

---
+++ plib/versioned_persistence.h
@@ -0,0 +1,41 @@
+//
+//  versioned_persistence.h
+//  vm_persistence
+//
+//  Created by Jinglei Ren on Apr. 1, 2015.
+//  Copyright (c) 2015 Jinglei Ren <jinglei@ren.systems>.
+//
+
+#ifndef VM_PERSISTENCE_PLIB_VERSIONED_PERSISTENCE_H_
+#define VM_PERSISTENCE_PLIB_VERSIONED_PERSISTENCE_H_
+
+#include <cstdint>
+
+namespace plib {
+
+struct DataEntry {
+  void *data;
+  int32_t size;
+};
+
+struct MetaEntry {
+  uint64_t address;
+  int32_t size;
+};
+
+class VersionedPersistence {
+ public:
+  virtual void *Submit(const DataEntry data[], int n) = 0;
+  virtual int Commit(void *handle, uint64_t timestamp,
+      const MetaEntry meta[], int16_t n) = 0;
+
+  virtual void **CheckoutPages(uint64_t timestamp, uint64_t addr[], int n) = 0;
+  virtual void DestroyPages(void *pages[], int n) = 0;
+
+  virtual ~VersionedPersistence() { }
+};
+
+} // namespace plib
+
+#endif // VM_PERSISTENCE_PLIB_VERSIONED_PERSISTENCE_H_
+
