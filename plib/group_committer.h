bfa12210ffd234aa1ada3e4a6664a3c85f800da7
Jinglei Ren <jinglei@ren.systems>
2015-05-17 21:35:55 -0700

    [plib] Grace closedown of flusher threads.

--- plib/group_committer.h
+++ plib/group_committer.h
@@ -31,6 +31,7 @@ template <typename DataEntry>
 class GroupCommitter : public VersionedPersistence<DataEntry> {
  public:
   GroupCommitter(const char *dev, int block_bits, int num_lanes, int size);
+  ~GroupCommitter();
 
   void *Submit(DataEntry data[], uint32_t n) { return data; }
   int Commit(void *handle, uint64_t timestamp,
@@ -46,8 +47,9 @@ class GroupCommitter : public VersionedPersistence<DataEntry> {
   GroupBuffer buffer_;
   std::vector<std::thread> flushers_;
   sem_t flush_sem_;
+  bool running_;
 
-  static void Flush(GroupBuffer *buffer, sem_t *flush_sem_);
+  static void Flush(GroupBuffer *buffer, sem_t *flush_sem_, bool *running);
 };
 
 template <typename DataEntry>
@@ -61,9 +63,22 @@ inline GroupCommitter<DataEntry>::GroupCommitter(
   }*/
 
   sem_init(&flush_sem_, 0, 0);
+  running_ = true;
   for (int i = 0; i < nlanes; ++i) {
-    flushers_.push_back(std::thread(Flush, &buffer_, &flush_sem_));
+    flushers_.push_back(std::thread(Flush, &buffer_, &flush_sem_, &running_));
   }
+}
+
+template <typename DataEntry>
+inline GroupCommitter<DataEntry>::~GroupCommitter() {
+  running_ = false;
+  for (size_t i = 0; i < flushers_.size(); ++i) {
+    sem_post(&flush_sem_);
+  }
+  for (std::thread &t : flushers_) {
+    t.join();
+  }
+  sem_destroy(&flush_sem_);
 }
 
 template <typename DataEntry>
@@ -77,28 +92,36 @@ inline int GroupCommitter<DataEntry>::Commit(void *handle, uint64_t timestamp,
   CRC32DataEncode(data_buf, timestamp, handle, data_size);
 
   sem_t commit_sem;
-  sem_init(&commit_sem, 0, 0);
+  int err = sem_init(&commit_sem, 0, 0);
+  if (err) {
+    perror("[Error] sem_init() in GroupCommitter::Commit():");
+    return err;
+  }
  
   int8_t *dest = buffer_.Lock(len, &flush_sem_);
   memcpy(dest, data_buf, len);
   buffer_.Release(dest, &commit_sem);
 
-  return sem_wait(&commit_sem);
+  err = sem_wait(&commit_sem);
+  sem_destroy(&commit_sem);
+  return err;
 }
 
 template <typename DataEntry>
-void GroupCommitter<DataEntry>::Flush(GroupBuffer *buffer, sem_t *flush_sem) {
+void GroupCommitter<DataEntry>::Flush(
+    GroupBuffer *buffer, sem_t *flush_sem, bool *running) {
   using namespace std::chrono;
   using microsec = duration<double, std::ratio<1,1000000>>;
 
   while (true) {
     sem_wait(flush_sem);
+    if (!*running) break;
     int8_t *mem;
     do {
       mem = buffer->BeginFlush();
       // TODO
       high_resolution_clock::time_point t1 = high_resolution_clock::now();
-      for (int i = 0; i < 100000; ++i) {}
+      for (int i = 0; i < 10000; ++i) {}
       high_resolution_clock::time_point t2 = high_resolution_clock::now();
       printf("flushed for %f usec\n", duration_cast<microsec>(t2 - t1).count());
     } while (buffer->EndFlush(mem));
