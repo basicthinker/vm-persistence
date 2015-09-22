749753d94aeed92582c7353293b53979df12bbab
Jinglei Ren <jinglei.ren@gmail.com>
2015-09-23 03:58:28 +0800

    [slib] Merge with YCSB-C and update slib code.

--- slib/hashtable.h
+++ slib/hashtable.h
@@ -10,24 +10,29 @@
 #define VM_PERSISTENCE_SLIB_HASHTABLE_H_
 
 #include <cstdint>
-#include <cassert>
+#include "slib/pair.h"
 #include "slib/list.h"
+#include "slib/vector.h"
 #include "slib/mem_alloc.h"
+#include "slib/assert.h"
 
 namespace slib {
 
 template<typename T>
 struct HashEqual {
+  __attribute__((transaction_safe))
   virtual uint64_t hash(const T &key) const {
     std::hash<T> hasher;
     return hasher(key);
   }
   
+  __attribute__((transaction_safe))
   virtual bool equal(const T &x, const T &y) const {
     std::equal_to<T> equal;
     return equal(x, y);
   }
   
+  __attribute__((transaction_safe))
   virtual ~HashEqual() { }
 };
 
@@ -38,13 +43,19 @@ struct hlist_bucket {
 
 template <typename K, typename V>
 struct hlist_pair {
-  K key;
-  V value;
+  K key() const { return pair.first; }
+  void set_key(const K &key) { pair.first = key; }
+  V value() const { return pair.second; }
+  void set_value(const V &value) { pair.second = value; }
+
   hlist_node node;
 
-  hlist_pair(const K &k, const V &v) : key(k), value(v) {
-    INIT_HLIST_NODE(&node);    
+  hlist_pair(const K &k, const V &v) : pair(k, v) {
+    INIT_HLIST_NODE(&node);
   }
+
+ private:
+  slib::pair<K, V> pair;
 };
 
 template <typename K, typename V, class HashEqual, class Alloc>
@@ -55,14 +66,21 @@ class hashtable {
   void set_local_load_factor(std::size_t f) { local_load_factor_ = f; }
   std::size_t bucket_count() const { return bucket_count_; }
   
-  bool find(const K &key, V &value) const;
-  bool update(const K &key, V &value);
-  bool insert(const K &key, const V &value);
-  bool erase(const K &key, std::pair<K, V> &erased);
-  std::vector<std::pair<K, V>> entries(const K *key = NULL,
-                                       std::size_t n = -1) const;
-  std::size_t clear();
-  void rehash(std::size_t buckets);
+  bool find(const K &key, V &value) const __attribute__((transaction_safe));
+
+  bool update(const K &key, V &value) __attribute__((transaction_safe));
+
+  bool insert(const K &key, const V &value) __attribute__((transaction_safe));
+
+  __attribute__((transaction_safe))
+  bool erase(const K &key, slib::pair<K, V> &erased);
+
+  slib::vector<slib::pair<K, V>> entries(const K *key = NULL,
+          std::size_t n = -1) const  __attribute__((transaction_safe));
+
+  std::size_t clear() __attribute__((transaction_safe));
+
+  void rehash(std::size_t buckets) __attribute__((transaction_safe));
   
   std::size_t size() const;
   std::size_t bucket_size(size_t i) const { return buckets_[i].size; }
@@ -83,7 +101,7 @@ class hashtable {
 
 // Accessory functions
 
-template <class Alloc>
+template <class Alloc> __attribute__((transaction_safe))
 inline hlist_bucket *new_buckets(std::size_t n) {
   hlist_bucket *bkts = (hlist_bucket *)Alloc::Malloc(n * sizeof(hlist_bucket));
   for (std::size_t i = 0; i < n; ++i) {
@@ -102,30 +120,32 @@ inline std::size_t size_sum(const hlist_bucket *bkts, std::size_t n) {
 }
 
 template <typename K, typename V, class HashEqual, class Alloc>
+__attribute__((transaction_safe))
 hlist_pair<K, V> *find_in(const hlist_bucket *bkt, const K &key,
     const HashEqual &hash_equal = HashEqual()) {
   hlist_node *node;
   hlist_for_each(node, &bkt->head) {
-    hlist_pair<K, V> *pair = container_of(node, &hlist_pair<K, V>::node);
-    if (hash_equal.equal(pair->key, key)) {
-      return pair;
+    hlist_pair<K, V> *kv_pair = container_of(node, &hlist_pair<K, V>::node);
+    if (hash_equal.equal(kv_pair->key(), key)) {
+      return kv_pair;
     }
   }
   return NULL;   
 }
 
 template <typename K, typename V>
-void insert_to(hlist_bucket *bkt, hlist_pair<K, V> *pair) {
-  hlist_add_head(&pair->node, &bkt->head);
+void insert_to(hlist_bucket *bkt, hlist_pair<K, V> *kv_pair) {
+  hlist_add_head(&kv_pair->node, &bkt->head);
   ++bkt->size;
 }
 
 template <typename K, typename V, class Alloc>
-void erase_from(hlist_bucket *bkt, hlist_pair<K, V> *pair) {
-  hlist_del(&pair->node);
-  Alloc::Delete(pair);
+__attribute__((transaction_safe))
+void erase_from(hlist_bucket *bkt, hlist_pair<K, V> *kv_pair) {
+  hlist_del(&kv_pair->node);
+  Alloc::Delete(kv_pair);
   --bkt->size;
-  assert((bool)bkt->size != hlist_empty(&bkt->head));
+  Assert((bool)bkt->size != hlist_empty(&bkt->head), "erase_from inconsistent");
 }
 
 template <typename K, typename V, class Alloc>
@@ -134,11 +154,11 @@ std::size_t clear_all(hlist_bucket *bkts, std::size_t n) {
   for (std::size_t i = 0; i < n; ++i) {
     hlist_node *pos, *next;
     hlist_for_each_safe(pos, next, &bkts[i].head) {
-      hlist_pair<K, V> *pair = container_of(pos, &hlist_pair<K, V>::node);
-      erase_from<K, V, Alloc>(bkts + i, pair);
+      hlist_pair<K, V> *kv_pair = container_of(pos, &hlist_pair<K, V>::node);
+      erase_from<K, V, Alloc>(bkts + i, kv_pair);
       ++num;
     }
-    assert(!bkts[i].size && hlist_empty(&bkts[i].head));
+    Assert(!bkts[i].size && hlist_empty(&bkts[i].head), "clear_all leaks");
   }
   return num;
 }
@@ -155,19 +175,19 @@ hashtable<K, V, HashEqual, Alloc>::hashtable(std::size_t n, std::size_t f) {
 template <typename K, typename V, class HashEqual, class Alloc>
 bool hashtable<K, V, HashEqual, Alloc>::find(const K &key, V &value) const {
   hlist_bucket *bkt = get_bucket(key);
-  hlist_pair<K, V> *pair = find_in<K, V, HashEqual, Alloc>(bkt, key);
-  if (!pair) return false;
-  value = pair->value;
+  hlist_pair<K, V> *kv_pair = find_in<K, V, HashEqual, Alloc>(bkt, key);
+  if (!kv_pair) return false;
+  value = kv_pair->value();
   return true;
 }
 
 template <typename K, typename V, class HashEqual, class Alloc>
 bool hashtable<K, V, HashEqual, Alloc>::update(const K &key, V &value) {
   hlist_bucket *bkt = get_bucket(key);
-  hlist_pair<K, V> *pair = find_in<K, V, HashEqual, Alloc>(bkt, key);
-  if (!pair) return false;
-  V old = pair->value;
-  pair->value = value;
+  hlist_pair<K, V> *kv_pair = find_in<K, V, HashEqual, Alloc>(bkt, key);
+  if (!kv_pair) return false;
+  V old = kv_pair->value();
+  kv_pair->set_value(value);
   value = old;
   return true;
 }
@@ -175,10 +195,10 @@ bool hashtable<K, V, HashEqual, Alloc>::update(const K &key, V &value) {
 template <typename K, typename V, class HashEqual, class Alloc>
 bool hashtable<K, V, HashEqual, Alloc>::insert(const K &key, const V &value) {
   hlist_bucket *bkt = get_bucket(key);
-  hlist_pair<K, V> *pair = find_in<K, V, HashEqual, Alloc>(bkt, key); 
-  if (pair) return false;
-  pair = Alloc::template New<hlist_pair<K, V>>(key, value);
-  insert_to(bkt, pair);
+  hlist_pair<K, V> *kv_pair = find_in<K, V, HashEqual, Alloc>(bkt, key); 
+  if (kv_pair) return false;
+  kv_pair = Alloc::template New<hlist_pair<K, V>>(key, value);
+  insert_to(bkt, kv_pair);
 
   if (bkt->size >= local_load_factor_) {
     rehash((bucket_count_ << 1) + 1);
@@ -187,20 +207,22 @@ bool hashtable<K, V, HashEqual, Alloc>::insert(const K &key, const V &value) {
 }
 
 template <typename K, typename V, class HashEqual, class Alloc>
-bool hashtable<K, V, HashEqual, Alloc>::erase(const K &key, std::pair<K, V> &erased) {
+bool hashtable<K, V, HashEqual, Alloc>::erase(
+    const K &key, slib::pair<K, V> &erased) {
   hlist_bucket *bkt = get_bucket(key);
-  hlist_pair<K, V> *pair = find_in<K, V, HashEqual, Alloc>(bkt, key);
-  if (!pair) return false;
-  erased.first = pair->key;
-  erased.second = pair->value;
-  erase_from<K, V, Alloc>(bkt, pair);
+  hlist_pair<K, V> *kv_pair = find_in<K, V, HashEqual, Alloc>(bkt, key);
+  if (!kv_pair) return false;
+  erased.first = kv_pair->key();
+  erased.second = kv_pair->value();
+  erase_from<K, V, Alloc>(bkt, kv_pair);
   return true;
 }
 
 template <typename K, typename V, class HashEqual, class Alloc>
-std::vector<std::pair<K, V>> hashtable<K, V, HashEqual, Alloc>::entries(
+slib::vector<slib::pair<K, V>> hashtable<K, V, HashEqual, Alloc>::entries(
     const K *key, std::size_t num) const {
-  std::vector<std::pair<K, V>> pairs;
+  if (num > bucket_count_) num = bucket_count_;
+  slib::vector<slib::pair<K, V>> pairs(num);
   hlist_bucket *bkt = key ? get_bucket(*key) : buckets_;
   hlist_pair<K, V> *pos = key ? find_in<K, V, HashEqual, Alloc>(bkt, *key) :
       container_of(bkt->head.first, &hlist_pair<K, V>::node);
@@ -211,8 +233,8 @@ std::vector<std::pair<K, V>> hashtable<K, V, HashEqual, Alloc>::entries(
   for (; bkt < bkt_end; ++bkt) {
     if (!node) node = bkt->head.first;
     for (; node; node = node->next) {
-      hlist_pair<K, V> *pair = container_of(node, &hlist_pair<K, V>::node);
-      pairs.push_back(std::make_pair(pair->key, pair->value));
+      hlist_pair<K, V> *kv_pair = container_of(node, &hlist_pair<K, V>::node);
+      pairs.push_back(slib::make_pair(kv_pair->key(), kv_pair->value()));
       if (pairs.size() == num) return pairs;
     }
   }
@@ -232,14 +254,15 @@ hashtable<K, V, HashEqual, Alloc>::~hashtable() {
 
 template <typename K, typename V, class HashEqual, class Alloc>
 void hashtable<K, V, HashEqual, Alloc>::rehash(std::size_t n) {
+  //Assert(false, "REHASH\n");
   hlist_bucket *bkts = new_buckets<Alloc>(n);
   std::size_t num = 0;
   for (std::size_t i = 0; i < bucket_count_; ++i) {
     hlist_node *pos, *next;
     hlist_for_each_safe(pos, next, &buckets_[i].head) {
-      hlist_pair<K, V> *pair = container_of(pos, &hlist_pair<K, V>::node);
-      std::size_t j = hash_equal_.hash(pair->key) % n;
-      insert_to(bkts + j, pair);
+      hlist_pair<K, V> *kv_pair = container_of(pos, &hlist_pair<K, V>::node);
+      std::size_t j = hash_equal_.hash(kv_pair->key()) % n;
+      insert_to(bkts + j, kv_pair);
       ++num;
     }
   }
--- slib/mem_alloc.h
+++ slib/mem_alloc.h
@@ -1,20 +1,33 @@
 //
 //  mem_alloc.h
+//  vm-persistence
 //
 //  Created by Jinglei Ren on 12/23/14.
 //  Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>.
 //
 
-#ifndef VM_PERSISTENCE_MEM_ALLOC_H_
-#define VM_PERSISTENCE_MEM_ALLOC_H_
+#ifndef VM_PERSISTENCE_SLIB_MEM_ALLOC_H_
+#define VM_PERSISTENCE_SLIB_MEM_ALLOC_H_
 
 #include <cstring>
-#include "sitevm/sitevm_malloc.h"
+#include "sitevm/sitevm.h"
+
+namespace slib {
 
 struct MemAlloc {
   static void *Malloc(std::size_t size) { return malloc(size); }
 
-  template <typename T>
+  template <typename T> __attribute__((transaction_pure))
+  static T *Realloc(T *old_pos, std::size_t old_size, std::size_t new_size) {
+    T *new_pos = (T *)Malloc(new_size);
+    if (old_pos) {
+      memcpy(new_pos, old_pos, old_size < new_size ? old_size : new_size);
+      Free(old_pos, old_size);
+    }
+    return new_pos;
+  }
+
+  template <typename T> __attribute__((transaction_pure))
   static void Free(T *p, std::size_t size) { free((void *)p); }
 
   template <typename T, typename... Arguments>
@@ -25,27 +38,40 @@ struct MemAlloc {
 };
 
 struct SvmAlloc {
-  static void *Malloc(std::size_t size) { return sitevm_malloc::smalloc(size); }
+  __attribute__((transaction_pure))
+  static void *Malloc(std::size_t size) {
+    __transaction_atomic{
+      return sitevm::smalloc(size);
+    }
+  }
 
-  template <typename T>
+  template <typename T> __attribute__((transaction_pure))
   static void Free(T *p, std::size_t size) {
-    memset((void *)p, 255, size);
-    sitevm_malloc::sfree((void *)p);
+    __transaction_atomic {
+      //memset((void *)p, 255, size);
+      sitevm::sfree((void *)p);
+    }
   }
 
-  template <typename T, typename... Arguments>
-  static T *New(Arguments... args) {
-    void *p = Malloc(sizeof(T));
-    ::new(p) T(args...);
-    return (T *)p;
+  template <typename T, typename... Args> __attribute__((transaction_pure))
+  static T *New(Args... args) {
+    __transaction_atomic {
+      void *p = Malloc(sizeof(T));
+      ::new(p) T(args...);
+      return (T *)p;
+    }
   }
 
-  template <typename T>
+  template <typename T> __attribute__((transaction_pure))
   static void Delete(T *p) {
-    p->~T();
-    Free(p, sizeof(T));
+    __transaction_atomic {
+      p->~T();
+      Free(p, sizeof(T));
+    }
   }
 };
 
-#endif // VM_PERSISTENCE_MEM_ALLOC_H_
+} // namespace slib
+
+#endif // VM_PERSISTENCE_SLIB_MEM_ALLOC_H_
 
